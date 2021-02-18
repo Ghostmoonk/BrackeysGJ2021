9cb5ac6e81e1fc56843c0e0e9fcb725f16c0e94f
thomas.blc <thomas.blanc@webwag.com>
2021-02-18 19:59:19 +0100

    trying to organize the project

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -43,7 +43,7 @@ public class Enemy : MonoBehaviour, IHealth
     int currentHealth;
 
     Vector3 fleeDirection;
-    EnemyState state;
+    public EnemyState state;
 
     #region Collisions
     [Header("Collision")]
